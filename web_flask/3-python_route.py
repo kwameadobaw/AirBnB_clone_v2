20cbdce4cd94c1e75e6a49b490f262cc9219d0c2
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2024-02-17 17:42:30 +0000

    Task 2 and 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -33,6 +33,7 @@ def c_text(text):
     return "C {}".format(text)
 
 
+@app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python_text(text):
     """Displays 'Python' followed by the value of <text>."""
