bbe6381a193852929bf312b547c594386dc5b46d
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2024-02-18 07:43:20 +0000

    Task 7

--- models/state.py
+++ models/state.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+import models
+from os import getenv
+from models.base_model import Base
+from models.city import City
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
@@ -11,3 +18,13 @@ class State(BaseModel):
         """String representation of the instance."""
         cls_name = self.__class__.__name__
         return "[{}] ({}) {}".format(cls_name, self.id, self.__dict__)
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def cities(self):
+            """Get a list of all related City objects."""
+            city_list = []
+            for city in list(models.storage.all(City).values()):
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
