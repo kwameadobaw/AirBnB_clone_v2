f01c93e534613dd0cb051ab93afdc4807f3fec4a
Kwame Adobaw <kwame.adobawjnr@gmail.com>
2024-02-18 07:32:26 +0000

    Task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -15,6 +15,8 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+app.jinja_env.trim_blocks = True
+app.jinja_env.lstrip_blocks = True
 
 
 @app.route("/", strict_slashes=False)
